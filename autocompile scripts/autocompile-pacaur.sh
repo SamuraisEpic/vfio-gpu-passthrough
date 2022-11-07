46929779798e66c11f9fe7e7ded78630790f4443
SamuraisEpic <69528792+SamuraisEpic@users.noreply.github.com>
2022-11-06 22:54:49 -0500

    modified:   autocompile scripts/autocompile-pacaur.sh (removed useless root check) 	modified:   autocompile scripts/autocompile-scream.sh (removed useless root check)

--- autocompile scripts/autocompile-pacaur.sh
+++ autocompile scripts/autocompile-pacaur.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-if [ "$EUID" -ne 0 ]
-  then echo "Please make sure you're running this script as root, or with sudo"
-  exit
-fi
 
 git clone https://aur.archlinux.org/pacaur.git
 cd pacaur
--- autocompile scripts/autocompile-scream.sh
+++ autocompile scripts/autocompile-scream.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-if [ "$EUID" -ne 0 ]
-  then echo "Please make sure you're running this script as root, or with sudo"
-  exit
-fi
 
 git clone https://github.com/duncanthrax/scream.git
 cd scream
